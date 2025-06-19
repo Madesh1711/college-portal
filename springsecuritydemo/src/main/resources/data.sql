de57def2362e01b74d3406ac03223dd5c43da6f4
Madesh <madeshwaran646@gmail.com>
2025-06-19 00:29:22 -0700

    Fix : Corrected hashed password

--- springsecuritydemo/src/main/resources/data.sql
+++ springsecuritydemo/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (username, password, role) VALUES
-('Admin', '$2a$10$zLVD.D0T6BHVHYvqUVSUn.9/OWBP4jsrL0hMTcl3hOXwIHHgGbh2W', 'ADMIN');
+('Admin','$2a$12$tbUK68JYFQo3bLfExoofyudOWvJp4iLoMOCYcSnrUuyx.TsZ9/.Ne', 'ADMIN');
